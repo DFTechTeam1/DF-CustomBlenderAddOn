089c836fb011ae9bdec9fffdaaf2be62d26f6af2
bastian-arman <putra.armananta@gmail.com>
2025-01-21 17:05:24 +0700

    feature(auto-organize-collection): modified custom prompt.

--- agent/custom_models.py
+++ agent/custom_models.py
@@ -1,44 +1,28 @@
 import sys
 from pathlib import Path
-from ollama import chat
+import ollama
 
 sys.path.append(str(Path(__file__).resolve().parents[1]))
 from langchain_core.prompts import PromptTemplate
-from agent.custom_template import organizer_template
-from pydantic import BaseModel
-from typing import Optional
+from custom_template import organizer_template
 
-from src.secret import Config
-
-config = Config()
-
-
-class ResponseDefault(BaseModel):
-    data: Optional[dict] = None
-
-
-object_data = [
-    "statue.001",
-    "statue_head.001",
-    "rooftop.001",
-    "watch.001",
-    "watch.002",
-    "chinesse_ornament.001",
-    "chinesse_ornament.002",
-    "tree.001",
-    "tree.002",
-    "tree.003",
-]
-
-prompt = PromptTemplate.from_template(template=organizer_template)
-formatted_prompt = prompt.format(object_data=str(object_data))
-
-
-response = chat(
-    messages=[{"role": "user", "content": formatted_prompt}],
-    model="llama3.2:3b-instruct-q2_K",
-    format=ResponseDefault.model_json_schema(),
+formatted_object_data = str(
+    [
+        "statue.001",
+        "statue_head.001",
+        "rooftop.001",
+        "watch.001",
+        "watch.002",
+        "chinesse_ornament.001",
+        "chinesse_ornament.002",
+        "tree.001",
+        "tree.002",
+        "tree.003",
+    ]
 )
+prompt = PromptTemplate.from_template(template=organizer_template)
+formatted_prompt = prompt.format(object_data=formatted_object_data)
 
-result = ResponseDefault.model_validate_json(response.message.content)
-print(result)
+response = ollama.generate(model="mistral:7b-instruct", prompt=formatted_prompt)
+
+print(response["response"])
